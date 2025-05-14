baef2ae8f81012f2149b0a20d3dff846ccd1c414
Gabriela <158316139+GabrielaSiqCap@users.noreply.github.com>
2025-05-14 17:44:05 +0000

    Realizando a Página Escolha

--- gabarita-ai/src/pages/Escolha.jsx
+++ gabarita-ai/src/pages/Escolha.jsx
@@ -1,37 +1,47 @@
-import Vestibulanda from '../assets/vestibulanda.jpg';
+import Vestibulanda from '../assets/vestibulanda.jpg'; 
 import Concurseiro from '../assets/concurseiro.jpg';
 import Notaalta from '../assets/notaalta.jpg';
 
 const Escolha = () => {
   return (
-    <div className='bg-warning'>
-{/*     
-    <div className='container bg-secondary w-50 h-50 "'>
-      <img className='w-2 h-3'src={Notaalta} alt="" />
-    <div className="badge w-50 h-80 bg-primary p-3 d-flex flex-column">
-      <h3>O que você procura?</h3>
-      <p className="p-1 fs-6 d-flex text-wrap">No nosso site, você escolhe o modo de estudo ideal para seu objetivo, com cronogramas flexíveis, simulados, videoaulas e acompanhamento personalizado.</p>
-    </div>
-    </div> */}
+    <div className='d-flex' style={{ height: '100vh' }}>
 
-    <div className='container d-flex align-items-center w-50 h-50'>
-      <img className='w-25 h-25' src={Vestibulanda} alt="" />
-    <div className='p-3 text-wrap'>
-    <h3>Vestibular</h3>
-      <p >Para vestibulandos, oferecemos materiais completos para ENEM e principais provas, incluindo resumos, exercícios, simulados e dicas de redação.</p>
-    </div>
-    </div>
-
-    <div className='container d-flex align-items-center w-50 h-50'>
-      <img className='w-25 h-25' src={Concurseiro} alt="" />
-      <div className='p-3 text-wrap'>
-      <h3>Concurseiro público</h3>
-      <p>Para concurseiros, temos conteúdos atualizados conforme os editais, questões comentadas, videoaulas e planos estratégicos para otimizar sua preparação e garantir sua aprovação!</p>
-      </div>
+      <div className='fundoesquerdo col-6 d-flex flex-column align-items-center justify-content-center'>
+        <img className='w-50 h-50 rounded-4' src={Notaalta} alt="" />
+        <div className='oqvcp p-3 rounded-4 w-50 h-5'>
+          <h3 className='subtitulo'>O que você procura?</h3>
+          <p className="p-1 fs-6 d-flex text-wrap text-light">
+            No nosso site, você escolhe o modo de estudo ideal para seu objetivo, com cronogramas flexíveis, simulados, videoaulas e acompanhamento personalizado.
+          </p>
+        </div>
       </div>
 
+      <div className='fundodireito col-6'>
+       <div className='container '>
+        <div className='container d-flex align-items-center my-4'>
+          <div className='conteudo p-3 text-wrap w-50'>
+            <h3>Vestibular</h3>
+            <p>
+              Para vestibulandos, oferecemos materiais completos para ENEM e principais provas, incluindo resumos, exercícios, simulados e dicas de redação.
+            </p>
+          </div>
+          <img className='w-25 h-25 rounded-4' src={Vestibulanda} alt="" />
+        </div>
+
+        <div className='container d-flex align-items-center my-4'>
+          <img className='w-25 h-25 rounded-4' src={Concurseiro} alt="" />
+          <div className='conteudo p-3 text-wrap w-50'>
+            <h3>Concurseiro público</h3>
+            <p>
+              Para concurseiros, temos conteúdos atualizados conforme os editais, questões comentadas, videoaulas e planos estratégicos para otimizar sua preparação e garantir sua aprovação!
+            </p>
+          </div>
+       </div>
+        </div>
+      </div>
+
     </div>
-  )
-}
+  );
+};
 
 export default Escolha;
