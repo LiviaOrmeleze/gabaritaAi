b61b5443a664be7cd6d38ca2c57dcd754d0dffb3
Livia Ormeleze <liviasenai.ormeleze@gmail.com>
2025-05-20 17:01:29 -0300

    Ajustando o home

--- gabarita-ai/src/App.jsx
+++ gabarita-ai/src/App.jsx
@@ -3,9 +3,10 @@ import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "bootstrap/dist/js/bootstrap.bundle.min"
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import Header from "./component/header/Header";
 import logo from '/logo.png'
-import pessoasHome from './assets/home.jpg'
+
 
 
 import "./scss/styles.scss"
@@ -17,12 +18,11 @@ const App = () => {
       <BrowserRouter>
       <Header  className='d-flex align-items-center'
       logo={logo}/>
-      <Home className="d-flex flex-column"
-      pessoasHome={pessoasHome}/>
+     
       
       <main className="flex-grow-1 d-flex flex-column">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home/>}  />
         
       </Routes>
       </main>
--- gabarita-ai/src/pages/Home.jsx
+++ gabarita-ai/src/pages/Home.jsx
@@ -1,16 +1,41 @@
-import React from 'react'
+import React from "react";
+import pessoasHome from "../assets/home.jpg";
+import banner from "../assets/banner.png";
 
-
-const Home = (props) => {
+const Home = () => {
   return (
-    <div >
-      <img  
-      height={400}
-      className='rounded-5 m-5 border border-2 border-dark'
-      src={props.pessoasHome} 
-      alt="" />
-    </div>
-  )
-}
+    <>
+      <div className="d-flex ">
+        <img
+          height={500}
+          width={500}
+          className="rounded-5 m-5  border border-2 border-dark"
+          src={pessoasHome}
+          alt=""
+        />
 
-export default Home
+        <div className="d-flex flex-column ">
+          <div className="blocoVesti p-3 text-center fs-5 mb-3 ">
+            Vestibular
+          </div>
+          <div className="blocoCon p-3 text-center  fs-5 mb-3">Concurso</div>
+          <div className="blocoAtua p-3 text-center fs-5">Atualidades</div>
+        </div>
+      </div>
+
+      <button className="w-25 rounded-5 p-2 btnCome text-light fs-5 position-fixed bottom-0 end-0 m-5">
+        Comece seu estudos <i class="bi bi-caret-right text-light"></i>
+      </button>
+
+      <img 
+      className="object-fit position-absolute end-0 m-3 "
+      height={200}
+      width={650}
+      src={banner}
+      alt="" />
+      <p></p>
+    </>
+  );
+};
+
+export default Home;
