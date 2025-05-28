3adad876a1b49e3175ad5d7507e30740a823a680
Gabriela <gabrielasiqueiracaputti@gmail.com>
2025-05-28 11:48:44 -0300

    Finalizado caixa conheça nosso site

--- gabarita-ai/src/pages/Conheca.jsx
+++ gabarita-ai/src/pages/Conheca.jsx
@@ -4,6 +4,7 @@ import folha from "../assets/folha.png";
 import logo from "../assets/logo.png";
 import estrela from "../assets/estrela.png";
 import Conhecendo from "../components/Conhecendo";
+import linhatorta from "../assets/linhatorta.png";
 
 const Conheca = () => {
   return (
@@ -14,8 +15,9 @@ const Conheca = () => {
       <div className="d-flex align-items-center flex-column">
         <img className="position-relative" src={folha} alt="" />
         <div className="position-absolute py-5 mt-3 ">
-          <h1 className="tituloconheca px-5">Conheça nosso site</h1>
-          <p className="body mt-5 px-5">
+          <h1 className="tituloconheca px-5 ms-4 py-3">Conheça nosso site</h1>
+          <img src={linhatorta} alt="" className="w-75 ms-5"/>
+          <p className="body mt-2 px-5">
             Siga o passo a passo, monte seu perfil e inicie seu processo de
             estudos
           </p>
