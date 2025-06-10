ebbd1884babdc5c3bcc2f9bdda5df9ffa4bf089e
Livia Ormeleze <liviasenai.ormeleze@gmail.com>
2025-06-10 11:16:29 -0300

    importando header

--- gabarita-ai/src/pages/PaginaPrincipal.jsx
+++ gabarita-ai/src/pages/PaginaPrincipal.jsx
@@ -3,10 +3,13 @@ import HomeOfc from '../components/HomeOfc'
 import EscolhaOf from '../components/EscolhaOf'
 import EstudarOfc from '../components/EstudarOfc'
 import SaibaMaisOfc from '../components/SaibaMaisOfc'
+import Header from '../components/Header'
+
 
 const PaginaPrincipal = () => {
   return (
     <div className='flex-column'>
+      <Header/>
         <HomeOfc />
       <EscolhaOf />
         <EstudarOfc />
