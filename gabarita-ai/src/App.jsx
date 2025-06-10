dbffb6efd457984b5de0e388de6948559f3420a9
Aninha <aninha.clara030807@gmail.com>
2025-06-10 08:16:07 -0300

    modificado os componentes

--- gabarita-ai/src/App.jsx
+++ gabarita-ai/src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Login from "./pages/Login";
 import Cadastro from "./pages/Cadastro";
-import Estudar from "./pages/Estudar";
 import Planos from "./pages/Planos";
 import Atualidades from "./pages/Atualidades";
 import Desempenho from "./pages/Desempenho";
@@ -23,16 +22,13 @@ import Sinonimos from "./pages/Sinonimos";
 import Conectivos from "./pages/Conectivos";
 import Prepositivos from "./pages/Prepositivos";
 
-
 import Escolha from "./pages/Escolha";
 import Conheca from "./pages/Conheca";
 import SaibaMais from "./pages/SaibaMais";
 import Cronograma from "./pages/Cronograma";
 
 const App = () => {
-
   return (
-    
     <BrowserRouter>
       <Header />
       <main className="flex-grow-1 d-flex flex-column">
@@ -40,26 +36,24 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/Dicionario" element={<Dicionario />} />
           <Route path="/Redacao" element={<Redacao />} />
-<Route path="/login" element={<Login />} />
-<Route path="/cadastro" element={<Cadastro />} />
-<Route path="/estudar" element={<Estudar />} />
-<Route path="/planos" element={<Planos />} />
-<Route path="/atualidades" element={<Atualidades />} />
-<Route path="/desempenho" element={<Desempenho />} />
-<Route path="/materias" element={<Materias />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/planos" element={<Planos />} />
+          <Route path="/atualidades" element={<Atualidades />} />
+          <Route path="/desempenho" element={<Desempenho />} />
+          <Route path="/materias" element={<Materias />} />
           <Route path="/significados" element={<Significados />} />
           <Route path="/sinonimos" element={<Sinonimos />} />
           <Route path="/conectivos" element={<Conectivos />} />
           <Route path="/prepositivos" element={<Prepositivos />} />
-        <Route path="/escolha" element={<Escolha />} />
-        <Route path="/conhecanossosite" element={<Conheca />} />
-        <Route path="/saibamais" element={<SaibaMais />} />
-        <Route path="/cronograma" element={<Cronograma />} />
-      </Routes>
+          <Route path="/escolha" element={<Escolha />} />
+          <Route path="/conhecanossosite" element={<Conheca />} />
+          <Route path="/saibamais" element={<SaibaMais />} />
+          <Route path="/cronograma" element={<Cronograma />} />
+        </Routes>
       </main>
     </BrowserRouter>
   );
 };
-  
-export default App;
 
+export default App;
